4d3349f088f9362bd1f6b1356a6a5315a629c2ca
Michael Jordan <mikesjordan@gmail.com>
2017-06-08 11:47:07 -0600

    good time to commit and push added post and routed it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
 import {AboutComponent} from "./components/about.component";
+import {PostComponent} from "./components/post.component";
 
 
-export const allAppComponents = [HomeComponent, AboutComponent];
+export const allAppComponents = [HomeComponent, AboutComponent, PostComponent];
 
 export const routes: Routes = [
+	{path: "post", component: PostComponent},
 	{path: "about", component: AboutComponent},
 	{path: "", component: HomeComponent},
 	{path: "**", redirectTo: ""}
